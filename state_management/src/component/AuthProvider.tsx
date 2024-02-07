9ed379365908f7e0e100485dc002fb5face77b86
kundan <kundanbhagat842@gmail.com>
2024-02-07 23:23:27 +0530

    custom provider added

---
+++ state_management/src/component/AuthProvider.tsx
@@ -0,0 +1,18 @@
+import { ReactNode, useDebugValue, useReducer } from "react";
+import authReducer from "../reducers/authReducer";
+import AuthContext from "../contexts/authContext";
+
+interface Props {
+  children: ReactNode;
+}
+const [user, dispatch] = useReducer(authReducer, "");
+
+const AuthProvider = ({ children }: Props) => {
+  return (
+    <AuthContext.Provider value={{ user, dispatch }}>
+      children
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
