110ca6617fe868cd1f214cb79467ece2ea6ec7b7
kundan <kundanbhagat842@gmail.com>
2024-02-10 23:23:40 +0530

    path param passed to routes and getting data from routes implemented

--- react-routers/src/components/UserDetailPage.tsx
+++ react-routers/src/components/UserDetailPage.tsx
@@ -1,4 +1,16 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  console.log("params", params);
+  const [searchParams, setSearchParams] = useSearchParams();
+  // this setSearchParams function can be used to upadate the query param of route
+  // but this fucntion have side effect, so should be used in event handler or useEfct
+  // a componet should be free of side effect
+  const location = useLocation();
+  console.log("search params", searchParams.toString());
+  console.log("name", searchParams.get("name"));
+  console.log("location", location);
   return <p>User</p>;
 };
 
--- react-routers/src/routing/router.tsx
+++ react-routers/src/routing/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../components/HomePage";
 import UserListPage from "../components/UserListPage";
 import ContactPage from "../components/ContactPage";
+import UserDetailPage from "../components/UserDetailPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
   { path: "/contact", element: <ContactPage /> },
+  { path: "/users/:usr_id", element: <UserDetailPage /> },
 ]);
 
 export default router;
